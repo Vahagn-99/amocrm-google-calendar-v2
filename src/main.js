9787521d485ba265e7a211dcbb886a46f4057a36
Vahagn-99 <vahagn99ghukasyan@gmail.com>
2023-09-22 16:49:44 +0400

    google calendar v2 left menu component

--- src/main.js
+++ src/main.js
@@ -4,20 +4,23 @@ import { handlePanel } from "./panel/config"
 import { handleEvents } from "./bind_actions/config"
 import { handleRender } from "./render/config"
 import { handleInit } from "./init/config"
+import { handleLeftMenu } from "./left_menu/config"
 import { async } from './helpers/helpers';
 
 window.Host = "https://widgets-api.dicitech.com/api/";
 
 const Widget = {
-    init: async (_amocrm, self,$) => await handleInit(self, $),
-    render: async (_amocrm,self, $) => {
-        await handleRender(_amocrm,self, $)
+    init: async (_amocrm, self, $) => await handleInit(self, $),
+    render: async (_amocrm, self, $) => {
+        await handleRender(_amocrm, self, $)
         return true
     },
-    bind_actions: async (_amocrm,self, $) => await handleEvents(self, $),
+    bind_actions: async (_amocrm, self, $) => await handleEvents(self, $),
     settings: async (appElement, self, $) => await handleSettings(appElement, self, $),
     advancedSettings: async (appElement, self, $) => await handlePanel(appElement, self, $),
     destroy: async (self, $) => true,
     onSave: async (self, $) => await async(window.APP),
+    initMenuPage: _bind(async (params) => await handleLeftMenu(params)
+    )
 }
 export default Widget;
